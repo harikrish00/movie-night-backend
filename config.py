5953600b7542e05a7da27742224d776e7f22cd1c
harikrish narasiman <Harikrishnan_Narasimhan@cable.comcast.com>
2016-12-18 17:54:29 -0500

    change env url for database

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class TestingConfig(Config):
     'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_PUCE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 config = {
     'development': DevelopmentConfig,
